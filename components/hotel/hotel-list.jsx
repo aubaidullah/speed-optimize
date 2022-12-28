f19ac9afea909109620677f4c5b2511b9683598e
Rahul Kumar <perfectrahul55@gmail.com>
2022-12-28 16:05:35 +0530

    update hotel list design

--- components/hotel/hotel-list.jsx
+++ components/hotel/hotel-list.jsx
@@ -71,51 +71,136 @@ const HotelList = ({hotels}) =>{
       };      
     
     
-      const exploreRender = hotels.map((item,i)=>{
-        return        <div key={i} className={tw`w-full lg:w-1/4 mb-4`}>
-            <Link href={`/hotel-${strToUrl(item.name)}-in-${item.cityname.toLocaleLowerCase()}-${item.id}`}>
-              <div className={tw`px-2`}>
-                  <div className="des_img t_rd" style={{margin:0}}>
-                      <img src={item.images} />
+      const exploreRender = hotels.map(function (item, i) {
+        var tmp = [];
+        var hurl =
+          "/hotel-" +
+          item.name.replace(/\s+/g, "-").toLowerCase() +
+          "-in-" +
+          item.cityname.replace(/\s+/g, "-").toLowerCase() +
+          "-" +
+          item.id;
+        for (var i = 0; i < item.ratings; i++) {
+          tmp.push(i);
+        }
+        var indents = tmp.map(function (i) {
+          return <i className="fa fa-star"></i>;
+        });
+        var tmp2 = [];
+  
+        for (var i = 0; i < 5 - item.ratings; i++) {
+          tmp2.push(i);
+        }
+        var star = tmp2.map(function (i) {
+          return <i className="fa fa-star-o"></i>;
+        });
+  
+        return (
+          <div key={i} className={tw`w-full lg:w-1/3 mb-4`}>
+            <div className={tw`col-sm-12 col-xs-12 _cr_mb px-4`} key={i}>
+              <div className="top_rated_box _box_shadow _bottom _hotel_shadow">
+                <div className="des_img">
+                  <img
+                    className="img-responsive"
+                    src={item.images.length > 0 ? item.images : "/logo-icon.png"}
+                    alt=""
+                  />
+                  <div className="content_">
+                    <div className="person_">
+                      <div className="item_list">
+                        {/* <i className="fa fa-user-o"></i> */}
+                        <span>1 Person</span>
+                      </div>
+                      <div className="item_list text-right">
+                        <span>{item.name} </span>
+                        <span>{item.locality} </span>
+                      </div>
+                    </div>
                   </div>
-                  <div className={tw`p-2 h_bottom_content`}>
-                      <h2 className={tw`font-bold`}>{item.name}</h2>
-                      <div className={tw`flex flex-wrap items-center mb-6`}>
-                          <IoLocationSharp/>
-                          {item.cityname}
-                      </div>
-                      <div className={tw`flex flex-wrap items-center`}>
-                          <BsStarHalf className="icon_size_h"/>
-                          <div className={tw`pl-1`}>{item.ratings}</div>
-                          
-                          <BsDot className={tw`inline`}/>
-                          12 Reviews
-                      </div>
-                      <div className={tw`border-t-1`}>
-                      <div className={tw`flex flex-wrap items-center`}>
-                          {
-                            
-                            item.price==0?<div className="price_inr">Price on Request</div>
-                            :<><div className={tw`font-bold price_inr`}>
-                              {item.price}/-
-                            </div>
-                            <div className={tw`pl-2`}>
-                              night
-                            </div></>
-                          
-                          }
-                          
-
-
-                      </div>
-                      </div>
+                </div>
+                <div className="royal-details">
+                  <h4>{item.name}</h4>
+                  <p className={tw`text-sm c_gray`}>{item.locality}</p>
+                </div>
+                <div className="price_List royal_list">
+                  <div className="cut_price price-cut text-right">
+                    <del></del>
                   </div>
-                  
+                  <div className={tw`price_tag flex items-cetner justify-bewteen`}>
+                    <Link href={hurl}>
+                      <a href={hurl} className="btn_anchor">
+                        View Details
+                      </a>
+                    </Link>
+                    {item.price > 0 ? (
+                      <>
+                        <span className="price_inr price_royal">
+                          {/* <i className="fa fa-inr"></i> */}
+                          {item.price}
+                        </span>
+                      </>
+                    ) : (
+                      <span className="price_inr price_royal royal-text">
+                        Price on Request
+                      </span>
+                    )}
+                  </div>
+                </div>
               </div>
-            </Link>
+            </div>
+          </div>
+        );
+      });
+
+
+    //   const exploreRender = hotels.map((item,i)=>{
+    //     return        <div key={i} className={tw`w-full lg:w-1/4 mb-4`}>
+    //         <Link href={`/hotel-${strToUrl(item.name)}-in-${item.cityname.toLocaleLowerCase()}-${item.id}`}>
+    //           <div className={tw`px-2`}>
+    //               <div className="des_img t_rd" style={{margin:0}}>
+    //                   <img src={item.images} />
+    //               </div>
+    //               <div className={tw`p-2 h_bottom_content`}>
+    //                   <h2 className={tw`font-bold`}>{item.name}</h2>
+    //                   <div className={tw`flex flex-wrap items-center mb-6`}>
+    //                       <IoLocationSharp/>
+    //                       {item.cityname}
+    //                   </div>
+    //                   <div className={tw`flex flex-wrap items-center`}>
+    //                       <BsStarHalf className="icon_size_h"/>
+    //                       <div className={tw`pl-1`}>{item.ratings}</div>
+                          
+    //                       <BsDot className={tw`inline`}/>
+    //                       12 Reviews
+    //                   </div>
+    //                   <div className={tw`border-t-1`}>
+    //                   <div className={tw`flex flex-wrap items-center`}>
+    //                       {
+                            
+    //                         item.price==0?<div className="price_inr">Price on Request</div>
+    //                         :<><div className={tw`font-bold price_inr`}>
+    //                           {item.price}/-
+    //                         </div>
+    //                         <div className={tw`pl-2`}>
+    //                           night
+    //                         </div></>
+                          
+    //                       }
+                          
+
+
+    //                   </div>
+    //                   </div>
+    //               </div>
+                  
+    //           </div>
+    //         </Link>
             
-        </div>
-    })
+    //     </div>
+    // })
+
+
+
 
 
     return    <div className="Shape_42">
