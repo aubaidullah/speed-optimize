992fcaaf042152605fb1502ee5148583fb142373
Rahul Kumar <perfectrahul55@gmail.com>
2023-02-10 17:32:06 +0530

    remove preact

--- next.config.js
+++ next.config.js
@@ -130,16 +130,16 @@ const nextConfig = {
       // destination_karde.htm
     ]
   },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      })
-    }
-    return config
-  },
+  // webpack: (config, { dev, isServer }) => {
+  //   if (!dev && !isServer) {
+  //     Object.assign(config.resolve.alias, {
+  //       react: 'preact/compat',
+  //       'react-dom/test-utils': 'preact/test-utils',
+  //       'react-dom': 'preact/compat',
+  //     })
+  //   }
+  //   return config
+  // },
   // async redirects(){
   //   return [
   //     {
