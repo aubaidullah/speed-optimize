327268440c21c17368b30bdfa797c6f543c0ebcd
Sumit Singh <sumit.kumar@dahatconsultancy.in>
2023-06-28 14:44:15 +0530

    [ADD] GTM & GA4 code added

--- components/meta.jsx
+++ components/meta.jsx
@@ -1,32 +1,55 @@
-import Head from 'next/head'
-import { useRouter } from 'next/router';
+import Head from "next/head";
+import { useRouter } from "next/router";
 // import Script from 'next/script';
-import * as Constants from "./Constants"
-const Meta = ({meta}) =>{
-    const { asPath, pathname } = useRouter();
-    console.log(asPath);
-    const GA_TRACKING_ID = "GTM-MT2JH48"
-    // const GA_TRACKING_ID = "G-CVJVT86DPD"
-    return <>
-    <Head>
+import * as Constants from "./Constants";
+
+const Meta = ({ meta }) => {
+  const { asPath, pathname } = useRouter();
+  console.log(asPath);
+  const GA_TRACKING_ID = "GTM-MT2JH48";
+  // const GA_TRACKING_ID = "G-CVJVT86DPD"
+  return (
+    <>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-MT2JH48');
+              `
+          }}
+        />
         <meta charset="utf-8" />
-        <link rel="icon" href={`${Constants.assets_api}/public/icons/logo.png`} />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
-        <meta 
-        httpEquiv="Content-Security-Policy"
-        content="upgrade-insecure-requests"
+        <link
+          rel="icon"
+          href={`${Constants.assets_api}/public/icons/logo.png`}
         />
-        <meta name="google-site-verification" content="9Sb4VP2cYN-uOYsvXZPSvdYFSlJ-8gg-K92mfDmIf6Q" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+        <meta
+          name="google-site-verification"
+          content="9Sb4VP2cYN-uOYsvXZPSvdYFSlJ-8gg-K92mfDmIf6Q"
+        />
         <meta name="theme-color" content="#f06726" />
-        <meta name="facebook-domain-verification" content="lmf2rs35govemhvoa9eitvy3mwv2b2" />
-        
-
-        <script 
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} 
-        // strategy="afterInteractive"
+        <meta
+          name="facebook-domain-verification"
+          content="lmf2rs35govemhvoa9eitvy3mwv2b2"
         />
 
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          // strategy="afterInteractive"
+        />
 
         {/* <Script>
 
@@ -39,8 +62,8 @@ const Meta = ({meta}) =>{
             `}
             </Script> */}
         <script
-            dangerouslySetInnerHTML={{
-                __html: `
+          dangerouslySetInnerHTML={{
+            __html: `
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
@@ -48,11 +71,11 @@ const Meta = ({meta}) =>{
                   page_path: window.location.pathname,
                 });
               `,
-            }}
+          }}
         />
 
         {/* </script> */}
-             {/* dangerouslySetInnerHTML = {{
+        {/* dangerouslySetInnerHTML = {{
                    __html: `
              window.dataLayer = window.dataLayer || [];
              function gtag(){dataLayer.push(arguments);}
@@ -64,38 +87,44 @@ const Meta = ({meta}) =>{
          }}
          /> */}
 
-        
-        
         <title>{meta.title}</title>
 
-        <link rel="canonical" href={`https://www.kiomoi.com${asPath}`}/>
+        <link rel="canonical" href={`https://www.kiomoi.com${asPath}`} />
         <meta name="description" content={meta.longDesc} />
         <meta name="keywords" content={meta.keywords} />
 
-        <meta property="og:title" content={meta.title}/>
-        <meta property="og:url" content={`https://www.kiomoi.com${asPath}`}/>
-        <meta property="og:site_name" content="thekiomoi"/>
-        <meta property="fb:admins" content="263867260781770"/>
-        <meta property="og:description" content={meta.longDesc}/>
-        <meta property="og:type" content="website"/>
-        <meta property="og:image" content={meta.image}/>
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:url" content={`https://www.kiomoi.com${asPath}`} />
+        <meta property="og:site_name" content="thekiomoi" />
+        <meta property="fb:admins" content="263867260781770" />
+        <meta property="og:description" content={meta.longDesc} />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content={meta.image} />
 
-        <meta name="twitter:card" content="summary"/>
-        <meta name="twitter:title" content={meta.title}/>
-        <meta name="twitter:description" content={meta.longDesc}/>
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.longDesc} />
         <meta name="twitter:url" content={`https://www.kiomoi.com${asPath}`} />
         <meta name="twitter:creator" content="@ki_omoi" />
         <meta name="twitter:site" content="@ki_omoi" />
-        <meta name="twitter:image" content={`${Constants.assets_api}/public/logo.png`}/>
+        <meta
+          name="twitter:image"
+          content={`${Constants.assets_api}/public/logo.png`}
+        />
         <meta name="robots" content="index" />
-        
 
-        <link rel="apple-touch-icon" href={`${Constants.assets_api}/public/icons/logo.png`} />
+        <link
+          rel="apple-touch-icon"
+          href={`${Constants.assets_api}/public/icons/logo.png`}
+        />
 
-        <link rel="manifest" href={`${Constants.assets_api}/public/manifest.json`} />
-    </Head>
-
+        <link
+          rel="manifest"
+          href={`${Constants.assets_api}/public/manifest.json`}
+        />
+      </Head>
     </>
-}
+  );
+};
 
-export default Meta
+export default Meta;
--- pages/_app.js
+++ pages/_app.js
@@ -1,83 +1,111 @@
-import 'nprogress/nprogress.css';
-import '@/styles/globals.css'
-import '@/styles/globals.css'
-import '@/styles/detail.css'
-import '@/styles/global.css'
-import '@/styles/Home.module.css'
-import '@/styles/homepage.css'
-import '@/styles/hotel.css'
-import '@/styles/leadform.css'
-import '@/styles/nav.css'
-import '@/styles/package.css'
-import '@/styles/qna.css'
-import '@/styles/travel-guide.css'
-import 'react-multi-carousel/lib/styles.css';
+import "nprogress/nprogress.css";
+import "@/styles/globals.css";
+import "@/styles/globals.css";
+import "@/styles/detail.css";
+import "@/styles/global.css";
+import "@/styles/Home.module.css";
+import "@/styles/homepage.css";
+import "@/styles/hotel.css";
+import "@/styles/leadform.css";
+import "@/styles/nav.css";
+import "@/styles/package.css";
+import "@/styles/qna.css";
+import "@/styles/travel-guide.css";
+import "react-multi-carousel/lib/styles.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import dynamic from 'next/dynamic';
-import NProgress from 'nprogress'
+import dynamic from "next/dynamic";
+import NProgress from "nprogress";
 
-import { wrapper } from '../redux_fx/store'
+import { wrapper } from "../redux_fx/store";
 
+NProgress.configure({ showSpinner: false, minimum: 0.2 });
 
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
+const ApolloProvider = dynamic(() =>
+  import("@apollo/client").then((mod) => mod.ApolloProvider)
+);
+const client = dynamic(() =>
+  import("@/components/Graphql/service").then((mod) => mod.client)
+);
+const Footer = dynamic(() => import("@/components/footer"));
+import Router from "next/router";
+import { useEffect } from "react";
 
-NProgress.configure({ showSpinner: false, minimum: 0.2 })
-
-Router.events.on('routeChangeStart', () => NProgress.start());
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
-
-
-
-const ApolloProvider = dynamic(() => import('@apollo/client').then((mod)=>mod.ApolloProvider))
-const client= dynamic(() => import('@/components/Graphql/service').then((mod)=>mod.client))
-const Footer = dynamic(() => import('@/components/footer'))
-import Router from 'next/router'
-import { useEffect } from 'react'
-
-
-NProgress.configure({ showSpinner: false, minimum: 0.2 })
+NProgress.configure({ showSpinner: false, minimum: 0.2 });
 // NProgress.set(0.4);
 
-Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on("routeChangeStart", () => NProgress.start());
 // NProgress.set(0.4)
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
-
-
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    dynamic(()=> import("react-facebook-pixel")
-    // import("react-facebook-pixel")
-      .then((x) => x.default)
-      .then((ReactPixel) => {
-        ReactPixel.init('630837885046995');
-        ReactPixel.pageView();
-
-        Router.events.on("routeChangeComplete", () => {
+    dynamic(() =>
+      import("react-facebook-pixel")
+        // import("react-facebook-pixel")
+        .then((x) => x.default)
+        .then((ReactPixel) => {
+          ReactPixel.init("630837885046995");
           ReactPixel.pageView();
-        });
-      }));
+
+          Router.events.on("routeChangeComplete", () => {
+            ReactPixel.pageView();
+          });
+        })
+    );
   });
   return (
     <>
-      <style jsx>{`
-        .model-content{
-          background-color:'red';
-        }
-      `}</style>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-MT2JH48"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
+
+      <style jsx>
+        {`
+          .model-content {
+            background-color: "red";
+          }
+        `}
+      </style>
+
       <ApolloProvider client={client}>
         <Component {...pageProps} />
       </ApolloProvider>
+      
       <Footer />
-    </>
-  )
 
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-CVJVT86DPD"
+      />
+
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){
+                    dataLayer.push(arguments);
+                }
+                gtag('js', new Date());
+                gtag('config', 'G-CVJVT86DPD');
+              `,
+        }}
+      />
+    </>
+  );
 }
 
 // export default MyApp
 
-export default wrapper.withRedux(MyApp)
+export default wrapper.withRedux(MyApp);
