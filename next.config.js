41160023bc1ee8cd0651e147bcbf937de291c5b7
Rahul Kumar <perfectrahul55@gmail.com>
2023-09-11 23:12:20 +0530

    update api

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   compress: true,
   reactStrictMode: true,
-  assetPrefix: "https:/assets.kiomoi.com",
+  assetPrefix: "https:/devassets.kiomoi.com",
   images: {
     domains: [
       "res.cloudinary.com",
