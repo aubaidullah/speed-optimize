f7e15c67a7ab5a304513c2dba7992e0736febaf8
Mac <mac@Macs-MacBook-Pro.local>
2023-09-16 15:22:07 +0530

    update description h1/anchor tag style

--- components/parseToHtml.jsx
+++ components/parseToHtml.jsx
@@ -1,6 +1,11 @@
 import ReactHtmlParser from "react-html-parser";
 const ParseHtml = ({ text }) => {
   // return ReactHtmlParser(text);
+  const [Tags, setTags] = useState("");
+  console.log(
+    "TEXT for parseHTML :- ",text
+  )
+
   return <div dangerouslySetInnerHTML={{__html:text}} />
   // return <div>{text}</div>
 };
