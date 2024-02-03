3036c6e14564aa2ef646fb6b07fc2945070ba534
Rahul Kumar <perfectrahul55@gmail.com>
2024-02-03 22:36:01 +0530

    update

--- components/Constants.jsx
+++ components/Constants.jsx
@@ -2,7 +2,7 @@
 // export const api = "https://admin.kiomoi.com";
 export const api = "https://admin.kiomoi.com"
 // export const assets_api = "http://localhost:3000"
-export const assets_api = "https://devassets.kiomoi.com";
+export const assets_api = "https://assets.kiomoi.com";
 // export const assets_api = "https://kiomoi.vercel.app";
 export const av = "1.3";
 export const pt = "WEBSITE";
